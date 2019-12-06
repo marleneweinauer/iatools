eb4b9cb0839941f2258370b29518107d116f8b28
Marlene Weinauer <marlene.weinauer@statistik.gv.at>
2019-12-06 13:32:46 +0100

    include test data

---
+++ R/data.R
@@ -0,0 +1,24 @@
+#' Dummy Survey Data 
+#'
+#' Created dummy Survey data of 4525 Observations of 141 Variables. 
+#' Each observation is an interview with a different person/household/enterprise/... . Values are answers given to survey questions. 
+#'
+#' @format A data table with 4525 Interviews of 141 Variables.
+#' \describe{
+#'   each variable is a dummy survey variable 
+#' }
+"survey_data_test"
+#> [1] "survey_data_test"
+
+
+#' Dummy Time Data 
+#'
+#' Created dummy Time data of 4525 Observations of 141 Variables. 
+#' Each observation is an interview with a different person/household/enterprise/... . Values are times needed on specific survey questions. 
+#'
+#' @format A data table with 4525 Interviews of 141 Variables.
+#' \describe{
+#'   each variable is a dummy survey variable 
+#' }
+"time_data_test"
+#> [1] "survey_data_test"
